7c5aa09bf15dc3cf7de01d01069a890c4e22c2fe
hackintoanetwork <83481196+hackintoanetwork@users.noreply.github.com>
2024-04-15 22:51:26 +0900

    Update tdata-migrate.py

--- tdata-migrate.py
+++ tdata-migrate.py
@@ -5,7 +5,6 @@ import shutil
 def migrate_telegram_tdata_mac(new_tdata_path):
     home = os.getenv('HOME')
     old_tdata_path = os.path.join(home, "Library/Application Support/Telegram Desktop/tdata")
-    
     if os.path.exists(old_tdata_path):
         try:
             shutil.rmtree(old_tdata_path)
@@ -13,7 +12,6 @@ def migrate_telegram_tdata_mac(new_tdata_path):
         except Exception as e:
             print(f"[-] An error occurred: {e}")
             return
-
     try:
         shutil.copytree(new_tdata_path, old_tdata_path)
         print(f"The directory {new_tdata_path} has been copied to {old_tdata_path}.")
@@ -25,7 +23,6 @@ def migrate_telegram_tdata_win(new_tdata_path):
     if home is None:
         print("[-] Error: USERPROFILE environment variable not found.")
         return
-
     old_tdata_path = os.path.join(home, "AppData\\Roaming\\Telegram Desktop\\tdata")
     print(old_tdata_path)
     if os.path.exists(old_tdata_path):
